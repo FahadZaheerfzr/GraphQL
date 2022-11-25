f730141778cf48c259a69ca3bfae20ff5ce28bc2
FahadZaheerfzr <fahadzaheer19@gmail.com>
2022-11-25 16:41:48 +0500

    Appolo server with typedefs and resolvers

---
+++ FakeData.js
@@ -0,0 +1,39 @@
+const UserList = [
+    {
+        id: 1,
+        name: 'John Doe',
+        username: 'johndoe',
+        age: 25,
+        nationality: 'United States'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        username: 'janedoe',
+        age: 22,
+        nationality: 'Canada'
+    },
+    {
+        id: 3,
+        name: 'John Smith',
+        username: 'johnsmith',
+        age: 30,
+        nationality: 'United Kingdom'
+    },
+    {
+        id: 4,
+        name: 'Jane Smith',
+        username: 'janesmith',
+        age: 28,
+        nationality: 'Poland'
+    },
+    {
+        id: 5,
+        name: 'Adam Smith',
+        username: 'adamsmith',
+        age: 35,
+        nationality: 'Scotland'
+    },
+]
+
+module.exports =  {UserList};
