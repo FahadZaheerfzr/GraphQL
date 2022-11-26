486af30a22c30138cc9107e79cbccf46e9cd33a6
FahadZaheerfzr <fahadzaheer19@gmail.com>
2022-11-26 13:08:31 +0500

    Parameterized query

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,10 +1,16 @@
 const {UserList} = require('../FakeData');
+const _ = require('lodash')
 
 const resolvers = {
     Query:{
         users(){
             return UserList;
+        },
+        user(_, args){
+            const {id} = args;
+            return _.find(UserList, {id});
         }
+
     }
 }
 
